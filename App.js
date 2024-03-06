0425b5469c2343b2b5c3d2f4cfd5665db08f7565
Samarth Bhosale <bhosalesamarth0@gmail.com>
2024-03-07 00:19:12 +0530

    JS AND JSX FUN

--- App.js
+++ App.js
@@ -10,7 +10,38 @@ const jsxHead = (<h1 id="jsxheading1">
     React JSX Foundation SaM Multiline
     </h1>); //Multiple line
 
+// Arrow function = Functional component with body
+const myFun = () => {
+    return <h1>this is arrow function of functional component</h1>
+};
+
+// Arrow function without body - direct return = Functional component
+const myFun1 = () => <h1>this is arrow function of functional component with direct return</h1>;
+
+
+const MyFun3 = () => <h1>COMPONENT COMPOSITION</h1>;
+const MyFun4 = () => <h2>JAVASCRIPT IN JSX</h2>;
+const myNum = 404;
+
+// Arrow function without body - direct return - multiline = Functional component
+const MyFun2 = () => (
+    <div>
+        {/* COMPONENT INSIDE COMPONENT - COMPONENT COMPOSITION */}
+        <MyFun3 />
+        {/* JAVASCRIPT INSIDE JSX */}
+        {MyFun4}
+        {MyFun4()}
+        <MyFun4></MyFun4>
+        
+        <h3>{myNum}</h3>
+        <h1>this is arrow function of functional component with direct return</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(heading);
-root.render(jsxHead);
+// root.render(jsxHead);
+
+// Rendering Functional Component
+root.render(<MyFun2 />);
